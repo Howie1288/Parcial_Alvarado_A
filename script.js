d03ab36b53e411a6ec6d9817bb925ed1ef392913
¨Howard <programadorhaaj.2023@gmail.com>
2023-07-18 11:08:44 -0600

    Atualizacciones de formato por que no pude cumplir con las tres formas

--- script.js
+++ script.js
@@ -3,14 +3,6 @@ const getCharacterButton = document.getElementById('get-character');
 const characterNameSelect = document.getElementById('character-name');
 const characterDataContainer = document.getElementById('character-data');
 
-const getLocationButton = document.getElementById('get-location');
-const locationIdSelect = document.getElementById('location-id');
-const locationDataContainer = document.getElementById('location-data');
-
-const getEpisodeButton = document.getElementById('get-episode');
-const episodeIdSelect = document.getElementById('episode-id');
-const episodeDataContainer = document.getElementById('episode-data');
-
 // Función para obtener el valor seleccionado de un select
 function getSelectedValue(selectElement) {
   return selectElement.value;
@@ -25,56 +17,45 @@ async function fetchData(url, container) {
     // Limpiar el contenedor antes de mostrar los nuevos datos
     container.innerHTML = '';
 
+    // Almacenar los nombres de personajes ya mostrados
+    const shownCharacters = new Set();
+
     // Procesar los datos según el tipo de endpoint
-    if (data.results) {
-      // Si la respuesta contiene una lista de resultados (personajes)
-      data.results.forEach((result) => {
+    data.results.forEach((result) => {
+      if (!shownCharacters.has(result.name)) {
+        shownCharacters.add(result.name);
+
         const characterElement = document.createElement('div');
-        characterElement.innerHTML = `
+        characterElement.classList.add('character-card');
+
+        const characterImage = document.createElement('img');
+        characterImage.src = result.image;
+        characterImage.alt = result.name;
+        characterElement.appendChild(characterImage);
+
+        const characterInfo = document.createElement('div');
+        characterInfo.classList.add('character-info');
+        characterInfo.innerHTML = `
           <h3>${result.name}</h3>
           <p>Status: ${result.status}</p>
           <p>Species: ${result.species}</p>
           <p>Gender: ${result.gender}</p>
         `;
+        characterElement.appendChild(characterInfo);
+
         container.appendChild(characterElement);
-      });
-    } else {
-      // Si la respuesta contiene un solo resultado (ubicación o episodio)
-      const infoElement = document.createElement('div');
-      infoElement.innerHTML = `
-        <h3>${data.name}</h3>
-        <p>${data.type || data.air_date}</p>
-      `;
-      container.appendChild(infoElement);
-    }
+      }
+    });
   } catch (error) {
     console.log('Error:', error);
   }
 }
 
-// Agregar event listener al botón para obtener los datos de personaje al hacer clic
+// Agregar event listener al botón para obtener los datos al hacer clic
 getCharacterButton.addEventListener('click', () => {
   const characterName = getSelectedValue(characterNameSelect);
   if (characterName) {
     const url = `https://rickandmortyapi.com/api/character/?name=${encodeURIComponent(characterName)}`;
     fetchData(url, characterDataContainer);
-  }
-});
-
-// Agregar event listener al botón para obtener los datos de ubicación al hacer clic
-getLocationButton.addEventListener('click', () => {
-  const locationId = getSelectedValue(locationIdSelect);
-  if (locationId) {
-    const url = `https://rickandmortyapi.com/api/location/${encodeURIComponent(locationId)}`;
-    fetchData(url, locationDataContainer);
-  }
-});
-
-// Agregar event listener al botón para obtener los datos de episodio al hacer clic
-getEpisodeButton.addEventListener('click', () => {
-  const episodeId = getSelectedValue(episodeIdSelect);
-  if (episodeId) {
-    const url = `https://rickandmortyapi.com/api/episode/${encodeURIComponent(episodeId)}`;
-    fetchData(url, episodeDataContainer);
   }
 });
